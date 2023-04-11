39f174d9d004eaf9532801610638d1fa50a85349
rpgtavernadeferro <116163153+rpgtavernadeferro@users.noreply.github.com>
2023-04-11 14:11:28 -0300

    Update main.2.0.py

--- main.2.0.py
+++ main.2.0.py
@@ -31,7 +31,7 @@ fig = go.Figure(data=
     ))
 
 #criação do grafico
-while contador_geral <3:
+while contador_geral <4:
     contador_geral +=1
 
     # criando listas gerais
@@ -47,15 +47,16 @@ while contador_geral <3:
     match contador_geral:
         case 1: qd = rdi(5,10) # planetas
         case 2: qd = rdi(100,200) # estrelas
-        case 3: qd = rdi(3,6) # naves       
+        case 3: qd = rdi(3,6) # naves
+        case 4: qd = rdi(3,3) # Assinatura energética, possíveis conflitos   
 
-    for i in range(qd):
+    for zero in range(qd):
         x_temp = rdu(1,6)
         y_temp = rdu(0,359)       
-        texto_temp = rdpalavra()
-         
+        texto_temp = rdpalavra()         
         
         match contador_geral:
+            
             case 1: # planetas
                 sz_temp = rdu(9,27)
                 variação_cor = rdi(31,41)
@@ -65,17 +66,29 @@ while contador_geral <3:
                 tipo_temp = 'Planeta '
                 texto.append(tipo_temp + texto_temp)
                 symbols ='circle' 
+                
             case 2: # estrelas
                 sz_temp = rdu(2,2)
                 rgb_temp = 'rgb(255,255,255)'
                 tipo_temp = ''
-                symbols ='circle'                
+                symbols ='circle'   
+                             
             case 3: # naves
                 sz_temp = rdu(12,12)
                 rgb_temp = 'rgb(0,'+ str(rdi(100,255)) +',0)'   
                 tipo_temp = 'Nave '
-                texto.append(tipo_temp + texto_temp)
+                classe_temp = rdi(1,12)
+                if rdi(1,5)>3: texto_temp = '????' 
+                texto.append(tipo_temp + texto_temp + ' (T' + str(classe_temp) + ')')
                 symbols ='arrow' 
+                
+            case 4: # Assinatura energética
+                sz_temp = rdu(8,8)
+                rgb_temp = 'rgb('+ str(rdi(50,255)) + ',0,0)'   
+                tipo_temp = 'Assinatura energética '
+                texto.append(tipo_temp + str(zero+1))
+                symbols ='hexagram'
+                                
         angulo.append(rdu(0,359))
         x.append(x_temp)
         y.append(y_temp)
